5100ac7a2b97e9c7c5c51412f2e684638b4a37ee
20120471 <truongvanhao159@gmail.com>
2022-10-28 15:37:07 +0700

    fix

--- Config/configLgFb.js
+++
@@ -1,7 +0,0 @@
-var configFb = {
-    "facebook_key": "1829476810737411", //Điền App ID của bạn vào đây
-    "facebook_secret": "7458f8c56a034605af73b9731626c8f7", //Điền App Secret ở đây
-    "callback_url": "http://localhost:5000/auth/facebook/callback"
-}
-
-module.exports = configFb
